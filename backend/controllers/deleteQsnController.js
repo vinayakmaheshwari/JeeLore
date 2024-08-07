5562b7b8539c7ca04bafdbc3fc54e6aa46eb45df
Vinayak <maheshwarivinayak90@gmail.com>
2024-08-07 23:11:01 +0530

    qsn delete update

--- backend/controllers/deleteQsnController.js
+++ backend/controllers/deleteQsnController.js
@@ -1,10 +1,13 @@
 import Qsn from "../models/qsnModel.js";
+import User from "../models/userModel.js";
+
 
 export const deleteQsn = async (req, res) => {
     try {
         const  qsnId  = req.body.qsnId.toString();
         const userId = req.user._id.toString();
         const qsn = await Qsn.findById(qsnId);
+        const user = await User.findById(userId);
         if (!qsn) {
             return res.status(404).json({ error: "Qsn not found" });
         }
@@ -14,6 +17,7 @@ export const deleteQsn = async (req, res) => {
         }
         
         await Qsn.deleteOne({ _id: qsnId });
+        await User.updateOne({ _id: userId }, { $pull: { qsnsUploaded: qsnId } });
         res.status(200).json({ msg: "Qsn deleted successfully" });
 
     } catch (error) {
