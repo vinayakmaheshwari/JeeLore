2ba25b35f264044d6a42143594d302e815144066
Vinayak <maheshwarivinayak90@gmail.com>
2024-08-16 15:56:50 +0530

    Update logoutController.js

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -1,6 +1,6 @@
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token","", {
+    res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "none",
