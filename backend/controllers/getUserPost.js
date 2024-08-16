d4a35c9b45ef3a063e147c19e22a48594a3dd40a
Vinayak <maheshwarivinayak90@gmail.com>
2024-08-16 15:44:11 +0530

    Update getUserPost.js

--- backend/controllers/getUserPost.js
+++ backend/controllers/getUserPost.js
@@ -5,7 +5,9 @@ export const getUserPost = async (req, res) => {
         const { id } = req.body
         const qsn = await Qsn.find({ postedBy: id }).sort({ createdAt: -1 });
         console.log(id)
-        res.status(200).json(qsn);
+        res.status(200).json({
+            qsn
+        });
     } catch (error) {
         console.log("error in getUserController", error);
         res.status(500).json({ error: "internal server error" });
