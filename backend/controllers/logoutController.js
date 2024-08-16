2cf95e4710d9ce6241e6d7693d226f717478bb2a
Vinayak <maheshwarivinayak90@gmail.com>
2024-08-16 15:49:07 +0530

    logout error

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -1,10 +1,16 @@
 export const logout = async (req, res) => {
-    try {
-        res.clearCookie("token", {path: '/'})
-        res.status(200).json({msg:"loggedout successfully"})
-    } catch (error) {
-        console.log("error in logoutController", error)
-        res.status(500).json({error:"internal server error"})
-    }
-  };
-  
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: "/",
+      domain: "jeelore.site",
+    });
+    res.status(200).json({ msg: "loggedout successfully" });
+  } catch (error) {
+    console.log("error in logoutController", error);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
