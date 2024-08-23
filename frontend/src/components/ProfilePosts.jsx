fb3e4f09127d052411201adcfa2b6bc6b28e290f
Vinayak <maheshwarivinayak90@gmail.com>
2024-08-23 17:33:10 +0530

    profile post

--- frontend/src/components/ProfilePosts.jsx
+++ frontend/src/components/ProfilePosts.jsx
@@ -1,26 +1,31 @@
-import { useContext, useEffect } from "react"
-import { AllContext } from "../../context/contex"
-
-
+import { useContext, useEffect } from "react";
+import { AllContext } from "../../context/contex";
 
 export const ProfilePosts = (props) => {
-    const auth = useContext(AllContext)
-    useEffect(() => {
-        try {
-            const res = fetch(`https://backend.jeelore.site/api/qsn/getuserposts`, {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    id: props.id
-                }),
-            })
-        } catch (error) {
-            
-        }
-    })
+  const auth = useContext(AllContext);
 
+  const getPosts = async () => {
+    try {
+      const res = fetch(`https://backend.jeelore.site/api/qsn/getUserPost`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: auth.auth._id,
+        }),
+      });
 
-}
+      console.log(auth.auth._id);
+    //   const data = await res.json();
+      console.log(res);
+    } catch (error) {
+      console.log(error, "error in profile Posts component");
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [auth.auth._id]);
+};
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,12 @@
-
+import { ProfilePosts } from "../components/ProfilePosts"
 
 
 export const ProfilePage = () => {
-
+   
     return(
         <div className="flex flex-col flex-1 h-full items-center justify-center w-full">
             <div className="h-20 w-full"></div>
+            <ProfilePosts/>
             <div className="flex h-full w-full items-center justify-center">
                 <p className="text-3xl mt-40 font-bold">Coming Soon</p>
             </div>
